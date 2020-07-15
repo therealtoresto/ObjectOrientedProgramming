c99dd99dbba97d21a779c213ccee14bdaab4b28d
TORESTO <therealtoresto@gmail.com>
2020-07-15 21:51:48 +0300

    abstraction EventEmitter example

---
+++ JavaScript/2-abstraction.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const { EventEmitter } = require('events');
+const { get } = require('http');
+const { inherits } = require('util');
+
+// Case 2: Active entity
+// for example: Collection, Stream, Scheduler
+
+class Collection extends EventEmitter {
+    constructor() {
+        super();
+        this.keys = [];
+        this.values = [];
+    }
+
+    set(key, value) {
+        const index = this.keys.indexOf(key);
+        if (index === -1) {
+            this.keys.push(key);
+            this.values.push(value);
+        } else {
+            const previous = this.values[index];
+            this.values[index] = value;
+            this.emit('rewrite', key, value, previous);
+        }
+    }
+    get(key) {
+        const index = this.keys.indexOf(key);
+        if (index === -1) return;
+        return this.values[index];
+    }
+}
+
+// Usage
+
+const collection = new Collection();
+collection.on('rewrite', (key, value,previous) => {
+    console.log('rewrite', { key, value, previous });
+});
+collection.set('name', 'Marcus');
+collection.set('name', 'Mao');
+const name = collection.get('name');
+console.dir({ name })
